bc19fd6ac81f0f41043f17be3ce54570461f4e55
Benjamin Kofi Yankey <benjamin.yankey@amalitech.com>
2026-02-23 15:15:37 +0000

    update

--- terraform/modules/security/main.tf
+++ terraform/modules/security/main.tf
@@ -45,6 +45,15 @@ resource "aws_security_group" "jenkins" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # SSH - for deployment to EC2 instances
+  egress {
+    description = "SSH outbound"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = {
     Name = "${var.project_name}-${var.environment}-jenkins-sg"
   }
